fea073bee7ff6680102de610a1702377388aa0d9
JoshuaGuevaraJ1 <l19371008@apizaco.tecnm.mx>
2025-05-23 00:29:24 +0000

    Added: Animations for modal.

--- src/components/ModalSettings.tsx
+++ src/components/ModalSettings.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+// ModalSettings.tsx
 
-import { Modal, ModalHeader, ModalBody, ToggleSwitch, ModalFooter, Button } from "flowbite-react"
-import { ThemeSelector } from "./ThemeSelector"
+// Modules
+import React, { useEffect, useState } from "react";
+
+// Components
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ToggleSwitch,
+  ModalFooter,
+  Button,
+} from "flowbite-react";
+import { ThemeSelector } from "./ThemeSelector";
 import { CloseIcon } from "../icons/Icons";
+
+// Hooks & context
 import { useAppSettings } from "../context/AppSettingsContext";
 
-// Context
 
 interface ModalSettingsProps {
     openModal: boolean;
@@ -15,47 +27,83 @@ interface ModalSettingsProps {
 const ModalSettingsComponent: React.FC<ModalSettingsProps> = ({openModal, setOpenModal}) => {
     const { grayscale, setGrayscale, animation, setAnimation } = useAppSettings();
 
-    return (
-        <Modal dismissible show={openModal} onClose={() => setOpenModal(false)} theme={{ content: { inner: grayscale ? 'grayscale' : '' }}} >
-        <ModalHeader>Configuraciones</ModalHeader>
-        <ModalBody>
-        <div className='overflow-auto space-y-2'>
-                <div className='border border-white/50 dark:border-gray-600/50 p-3 rounded-2xl space-y-2'>
-                    <h4 className='font-semibold text-gray-500 dark:text-gray-100 text-lg'>Efectos Visuales</h4>
-                    <div className='flex items-center'>
-                        <h6>✨ Animaciones de fondo</h6>
-                        <div className='ml-auto'>
-                            <ToggleSwitch checked={animation} onChange={setAnimation} sizing='lg' />
-                        </div>
-                    </div>
-                </div>
+    // Add animations
+    const [shouldRender, setShouldRender] = useState(openModal);
+    const [animate, setAnimate] = useState(false);
 
-                <div className='border border-white/50 dark:border-gray-600/50 p-3 rounded-2xl space-y-2'>
-                    <h4 className='font-semibold text-gray-500 dark:text-gray-100 text-lg'>Accesibilidad</h4>
-                    <div className='flex items-center'>
-                        <h6>🔳 Modo escala de grises</h6>
-                        <div className='ml-auto'>
-                            <ToggleSwitch checked={grayscale} onChange={setGrayscale} sizing='lg' />
+    useEffect(() => {
+        if (openModal) {
+        setShouldRender(true);
+        setTimeout(() => setAnimate(true), 40);
+        } else {
+        setAnimate(false);
+        const timeout = setTimeout(() => setShouldRender(false), 300);
+        return () => clearTimeout(timeout);
+        }
+    }, [openModal]);
+
+    if (!shouldRender) return null;
+
+    return (
+        <Modal
+            dismissible
+            show={true}
+            onClose={() => setOpenModal(false)}
+            theme={{
+                root: {show: {on:  animate ? 'opacity-100': 'opacity-0'}},
+                content: {inner: `${animate ? 'opacity-100 translate-y-0 scale-100' : 'opacity-0 translate-y-10 scale-95'} ${grayscale ? "grayscale" : ""}`,},
+            }}
+            
+        >
+            <ModalHeader>Configuraciones</ModalHeader>
+            <ModalBody>
+                <div className="overflow-auto space-y-2">
+                    <div className="border border-white/50 dark:border-gray-600/50 p-3 rounded-2xl space-y-2">
+                        <h4 className="font-semibold text-gray-500 dark:text-gray-100 text-lg">Efectos Visuales</h4>
+                        <div className={`flex items-center transition-all ease-in-out duration-500 ${animate ? 'opacity-100 translate-y-0 delay-500':'opacity-0 translate-y-8'}`}>
+                            <h6>✨ Animaciones de fondo</h6>
+                            <div className="ml-auto">
+                            <ToggleSwitch
+                                checked={animation}
+                                onChange={setAnimation}
+                                sizing="lg"
+                            />
+                            </div>
                         </div>
                     </div>
-                  
-                    <div className='flex items-center'>
-                        <h6>🎨 Tema</h6>
-                        <div className='ml-auto'>
+
+                    <div className="border border-white/50 dark:border-gray-600/50 p-3 rounded-2xl space-y-2">
+                        <h4 className="font-semibold text-gray-500 dark:text-gray-100 text-lg">Accesibilidad</h4>
+                        <div className={`flex items-center transition-all ease-in-out duration-500 ${animate ? 'opacity-100 translate-y-0 delay-500':'opacity-0 translate-y-8'}`}>
+                            <h6>🔳 Modo escala de grises</h6>
+                            <div className="ml-auto">
+                            <ToggleSwitch
+                                checked={grayscale}
+                                onChange={setGrayscale}
+                                sizing="lg"
+                            />
+                            </div>
+                        </div>
+
+                        <div className={`flex items-center transition-all ease-in-out duration-500 ${animate ? 'opacity-100 translate-y-0 delay-500':'opacity-0 translate-y-8'}`}>
+                            <h6>🎨 Tema</h6>
+                            <div className="ml-auto">
                             <ThemeSelector />
+                            </div>
                         </div>
                     </div>
                 </div>
-              </div>
-        </ModalBody>
-        <ModalFooter>
-          <div className="w-full flex justify-end space-x-2">
-            <Button onClick={() => setOpenModal(false)} className="gap-2" aria-label="Cerrar configuración"><CloseIcon height={20} width={20}/>Cerrar</Button>
-          </div>
-          
-        </ModalFooter>
-      </Modal>
-    )
-}
+            </ModalBody>
+            <ModalFooter>
+                <div className="w-full flex justify-end space-x-2">
+                    <Button onClick={() => setOpenModal(false)} className="gap-2" aria-label="Cerrar configuración">
+                        <CloseIcon height={20} width={20} />
+                        Cerrar
+                    </Button>
+                </div>
+            </ModalFooter>
+       </Modal>
+    );
+};
 
 export const ModalSettings = React.memo(ModalSettingsComponent);
